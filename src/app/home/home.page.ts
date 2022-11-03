acd874b499d4b8043ed9671b99125a2be7b7d4ce
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-11-03 12:21:05 +0100

    ntn

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  features: any[] = [
+    {id: 1, name: 'Top Up', src: 'assets/icons/top-up.png', background: 'rgba(27,150,181, 0.1)', page: ''},
+    {id: 2, name: 'Withdraw', src: 'assets/icons/cash-withdrawal.png', background: 'rgba(106,100,255, 0.1)', page: ''},
+    {id: 3, name: 'Send', src: 'assets/icons/send.png', background: 'rgba(255, 196, 9, 0.1)', page: ''},
+    {id: 4, name: 'Pay', src: 'assets/icons/debit-card.png', background: 'rgba(27,150,181, 0.1)', page: ''},
+  ];
+
   constructor() {}
 
 }
