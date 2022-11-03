6172be965e324027a6692e7f880289fff58ab202
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-11-03 12:25:42 +0100

    ntn

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,6 +14,11 @@ export class HomePage {
     {id: 4, name: 'Pay', src: 'assets/icons/debit-card.png', background: 'rgba(27,150,181, 0.1)', page: ''},
   ];
 
+  transactions: any[] = [
+    {id: 1, vendor: 'Received from PhonePe', image: '', amount: 1500, time: '3:00PM'},
+    {id: 2, vendor: 'Flaticons', image: '', amount: -1200, time: '4:00PM'}
+  ];
+
   constructor() {}
 
 }
